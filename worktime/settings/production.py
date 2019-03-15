15ba6597103ae426ab67e68e75d304a23916d4fc
Max Weiss <30f0fn@gmail.com>
2019-03-15 14:51:41 -0400

    fix production settings

--- worktime/settings/production.py
+++ worktime/settings/production.py
@@ -7,13 +7,3 @@ SECRET_KEY=os.environ.get('SECRET_KEY')
 import django_heroku
 django_heroku.settings(locals())
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE' : 'django.db.backends.postgresql_psycopg2',
-#         'NAME' : 'worktime',
-#         'USER' : 'mw',
-#         'PASSWORD' : '30f0fnts',
-#         'HOST' : 'localhost',
-#         'PORT' : '',
-#     }
-# }
