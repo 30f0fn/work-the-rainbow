a8af54099e4a30610c1f7cda6b3ac7ec441c69d3
Max Weiss <30f0fn@gmail.com>
2019-03-15 15:10:27 -0400

    trying to configure db for heroku

--- worktime/settings/production.py
+++ worktime/settings/production.py
@@ -6,7 +6,8 @@ SECRET_KEY=os.environ.get('SECRET_KEY')
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 
-DATABASE_URL = 'postgresql:///gentle-wildwood-51939'
+# DATABASE_URL = 'postgres://yfgrudrwvxsmzw:bfcfdf239cc077055d636616c5d3e7d3bc71a1b2636c4a4a90ad2cbca080a1d0@ec2-107-20-177-161.compute-1.amazonaws.com:5432/da0c2l3reqocrf'
+# 'postgresql:///gentle-wildwood-51939'
 
 DATABASES = {'default' : dj_database_url.config()}
 
