508cd320643532d67be5f31fb560485fb5baa08c
Max Weiss <30f0fn@gmail.com>
2019-03-15 15:01:31 -0400

    try to configure db

--- worktime/settings/production.py
+++ worktime/settings/production.py
@@ -5,7 +5,10 @@ SECRET_KEY=os.environ.get('SECRET_KEY')
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+
+DATABASE_URL = 'postgresql:///worktime'
+
+DATABASES = {'default' : dj_database_url.config()}
 
 import django_heroku
 django_heroku.settings(locals())
