146ffd9848014fdb96808917098c7eb5fa5c3576
Max Weiss <30f0fn@gmail.com>
2019-04-15 06:30:22 -0400

    hm

--- worktime/settings/production.py
+++ worktime/settings/production.py
@@ -33,9 +33,6 @@ INTERNAL_IPS = ['127.0.0.1']
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 
-# DATABASE_URL = 'postgres://yfgrudrwvxsmzw:bfcfdf239cc077055d636616c5d3e7d3bc71a1b2636c4a4a90ad2cbca080a1d0@ec2-107-20-177-161.compute-1.amazonaws.com:5432/da0c2l3reqocrf'
-# 'postgresql:///gentle-wildwood-51939'
-
 DATABASES = {'default' : dj_database_url.config()}
 
 import django_heroku
