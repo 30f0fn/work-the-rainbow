f131b4a6b4b99aaedfcaa752a841b9bb17503fe7
Max Weiss <30f0fn@gmail.com>
2019-05-01 09:08:52 -0400

    add parent

--- main/models.py
+++ main/models.py
@@ -715,26 +715,33 @@ class ShiftAssignmentCollection(models.Model):
             break
         return retval
 
-
     def create_commitments(self):
         shifts = Shift.objects.filter(classroom=self.period.classroom)
         assignments = self.shiftassignment_set.all()
+        offset = 0
         for sh in shifts:
+            offset = offset + 1
+            available_indices = [(offset + i) % 4 for i in range(4)]
+            # instead of cycling the indices, keep a count of how many times an index has been used, then always pick the least-used index first
             families = [assignment.child for assignment in assignments
                         if assignment.shift == sh]
             sh_occs = list(sh.occurrences_for_date_range(self.period.start,
                                                          self.period.end))
             assert(sum([child.shifts_per_month for child in families]) <= 4)
-            families.sort(key = lambda c : c.shifts_per_month)
-            available_indices = [0, 1, 2, 3]
+            families.sort(key = lambda c : -c.shifts_per_month)
+            # so either [0,1,2,3] or [1,2,3,4]
             def assign_from_index(child, index):
                 for occ in sh_occs[index::4]:
                     commitment = occ.create_commitment(child)
             for child in families:
-                first_index = min(available_indices)
+                first_index = available_indices.pop()
                 assign_from_index(child, first_index)
+                available_indices.remove(first_index)
                 if child.shifts_per_month == 2:
-                    assign_from_index(child, first_index+2)
+                    second_index = (first_index + 2) % 4
+                    # must be available, since child has at most one predecessor (else sum of children's shifts per months exceeds four), and that predecessor has different parity 
+                    available_indices.remove(second_index)
+                    assign_from_index(child, second_index)
 
 
 # def create_commitments(self):
